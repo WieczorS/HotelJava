da0115054cace08d682f403b92f3a9c50c5b26d2
Jakub Wieczorek <j.kuba.wieczorek1@gmail.com>
2024-03-16 17:19:56 +0100

    .

--- src/main/java/pl/wsb/hotel/Room.java
+++ src/main/java/pl/wsb/hotel/Room.java
@@ -2,15 +2,31 @@ package pl.wsb.hotel;
 
 import java.time.LocalDate;
 
-public class Room {
-public int id;
-public String description;
-public double area;
-public int floor;
-public Boolean hasKingSizeBed;
-public Boolean hasBalcony;
-public Boolean hasBathroom;
-public Boolean hasTV;
-public Boolean hasAirConditioning;
 
+// fields  //
+
+public class Room {
+    public int id;
+    public String description;
+    public double area;
+    public int floor;
+    public Boolean hasKingSizeBed;
+    public Boolean hasBalcony;
+    public Boolean hasBathroom;
+    public Boolean hasTV;
+    public Boolean hasAirConditioning;
+
+    // constructors  //
+    public Room(int id, String description, double area, int floor, Boolean hasKingSizeBed, Boolean hasBalcony,
+                Boolean hasBathroom, Boolean hasTV, Boolean hasAirConditioning) {
+        this.id = id;
+        this.description = description;
+        this.area = area;
+        this.floor = floor;
+        this.hasKingSizeBed = hasKingSizeBed;
+        this.hasBalcony = hasBalcony;
+        this.hasBathroom = hasBathroom;
+        this.hasTV = hasTV;
+        this.hasAirConditioning = hasAirConditioning;
+    }
 }
